56fd256d3c46023f4cc961c1b484cc1b4f5e0f10
Grace Dill <60298734+Tiny-Pickles@users.noreply.github.com>
2021-12-16 17:22:11 -0500

    made a lot of edits added networks to other pages made some small edits to css.

---
+++ Webpage-HTML/web_session_leaf/styles.js
@@ -0,0 +1,80 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.9.0",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "shape" : "ellipse",
+      "background-opacity" : 1.0,
+      "background-color" : "rgb(199,233,192)",
+      "border-color" : "rgb(204,204,204)",
+      "color" : "rgb(0,0,0)",
+      "font-size" : 16,
+      "font-family" : "SansSerif.plain",
+      "font-weight" : "normal",
+      "width" : 38.0,
+      "text-opacity" : 1.0,
+      "height" : 38.0,
+      "border-opacity" : 1.0,
+      "border-width" : 0.0,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "content" : "data(name)"
+    }
+  }, {
+    "selector" : "node[leaftype = 'leaf type']",
+    "css" : {
+      "shape" : "roundrectangle"
+    }
+  }, {
+    "selector" : "node[leaftype = 'leaf type']",
+    "css" : {
+      "background-color" : "rgb(247,243,154)"
+    }
+  }, {
+    "selector" : "node[leaftype = 'leaf type']",
+    "css" : {
+      "font-size" : 20
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "source-arrow-color" : "rgb(0,0,0)",
+      "line-style" : "solid",
+      "text-opacity" : 1.0,
+      "font-size" : 10,
+      "target-arrow-shape" : "none",
+      "font-family" : "Dialog.plain",
+      "font-weight" : "normal",
+      "line-color" : "rgb(132,132,132)",
+      "opacity" : 1.0,
+      "width" : 2.0,
+      "content" : "",
+      "target-arrow-color" : "rgb(0,0,0)",
+      "color" : "rgb(0,0,0)",
+      "source-arrow-shape" : "none"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Coniferous']",
+    "css" : {
+      "line-color" : "rgb(222,143,78)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Deciduous']",
+    "css" : {
+      "line-color" : "rgb(109,155,195)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
---
+++ Webpage-HTML/web_session_status/styles.js
@@ -0,0 +1,80 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.9.0",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "background-color" : "rgb(199,233,192)",
+      "height" : 35.0,
+      "shape" : "ellipse",
+      "color" : "rgb(0,0,0)",
+      "width" : 35.0,
+      "font-size" : 14,
+      "background-opacity" : 1.0,
+      "font-family" : "SansSerif.plain",
+      "font-weight" : "normal",
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "border-width" : 0.0,
+      "border-color" : "rgb(204,204,204)",
+      "text-opacity" : 1.0,
+      "border-opacity" : 1.0,
+      "content" : "data(name)"
+    }
+  }, {
+    "selector" : "node[Column_4 = 'status']",
+    "css" : {
+      "background-color" : "rgb(236,226,240)"
+    }
+  }, {
+    "selector" : "node[Column_4 = 'status']",
+    "css" : {
+      "shape" : "roundrectangle"
+    }
+  }, {
+    "selector" : "node[Column_4 = 'status']",
+    "css" : {
+      "font-size" : 20
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "source-arrow-shape" : "none",
+      "line-color" : "rgb(132,132,132)",
+      "content" : "",
+      "text-opacity" : 1.0,
+      "width" : 2.0,
+      "target-arrow-color" : "rgb(0,0,0)",
+      "target-arrow-shape" : "none",
+      "opacity" : 1.0,
+      "font-family" : "Dialog.plain",
+      "font-weight" : "normal",
+      "line-style" : "solid",
+      "source-arrow-color" : "rgb(0,0,0)",
+      "font-size" : 10,
+      "color" : "rgb(0,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Coniferous']",
+    "css" : {
+      "line-color" : "rgb(222,143,78)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Deciduous']",
+    "css" : {
+      "line-color" : "rgb(109,155,195)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
